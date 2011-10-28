936074e487847a75b44c99c2c2c3b3bc8ec9afcf
Jonathan <ksk.logan@hotmail.com>
2011-10-28 16:47:56 -0700

    Engine and I/O working, need to implement minimax.

--- a3.hs
+++ a3.hs
@@ -1,4 +1,7 @@
-import Control.Monad.Trans
+-- Learning Haskell's I/O is not trivial, and it's not the point of this
+-- assignment, so use a little "evil" for the greater good.
+import System.IO.Unsafe
+
 -- Point is an x-y coordinate on the board.
 data Point = Point (Int, Int) | NullPoint deriving (Eq, Show)
 
@@ -21,8 +24,8 @@ data BoardState = BoardState ( [Point], [Point] ) Bool Int
 -- a white pawn, black pawn or is empty, respectively.
 instance Show BoardState where
     show (BoardState (wp, bp) wt d) =
-        printBoard pp d d 
-        where ap = [Point (x, y) | y <- [0..d], x <- [0..d]]
+        (printBoard pp d d) ++ "\n" 
+        where ap = [Point (x, y) | y <- reverse [0..d], x <- [0..d]]
               pp = [if (elem p wp || elem p bp)
                         then (if elem p wp then "W"
                                            else "B")
@@ -51,11 +54,18 @@ genBoard n
 
 -- Play hexapawn with given board. If the game is over, print the winner.
 -- Otherwise, get either the user's move or the AIs move.
+test :: BoardState -> String
+test b@(BoardState ps wt d)
+    | boardUtility b == 999   = "Black wins."
+    | boardUtility b == -999  = "White wins."
+    | wt == True              = hexapawn (playerMove b (if wt then 'W' else 'B'))
+    | otherwise               = hexapawn (playerMove b (if wt then 'W' else 'B'))
+    
 hexapawn :: BoardState -> String
 hexapawn b@(BoardState ps wt d)
     | boardUtility b == 999   = "Black wins."
     | boardUtility b == -999  = "White wins."
-    | wt == True              = hexapawn (playerMove b)
+    | wt == True              = hexapawn (playerMove b 'W')
     | otherwise               = hexapawn (minimax b)
 
 -- Start a game of hexapawn on a NxN board. Disallow boards smaller than
@@ -74,31 +84,35 @@ boardUtility (BoardState (wp, bp) wt d)
     | null bp                             = -999
     | not (null [p | p <- bp, py p == d]) = 999
     | not (null [p | p <- wp, py p == 0]) = -999
-    | null (legalMoves wp bp 'W' d)       = 999
-    | null (legalMoves bp wp 'B' d)       = -999
+    | null (map (legalMoves bp 'W' d) wp) = 999
+    | null (map (legalMoves wp 'B' d) bp) = -999
     | otherwise                           = 0
 
 
 -- Return a list of all legal moves that can be made by the given player on
 -- the given BoardState. ap is a list of all Points on the given board and
 -- am is all the points not occupied.
-legalMoves pp op pl d = [p | p <- concat pm, p /= NullPoint]
+legalMoves op pl d (Point (x,y)) = [p | p <- pm, p /= NullPoint]
     where v  = if pl == 'W' then (-1) else (1)
-          pm = map (\(Point (x, y)) -> 
-                        (if ((elem (Point (x, y+v)) op)
-                            || y+v < 0 || y+v > d) then NullPoint else (Point (x, y+v))) :
-                        (if ((elem (Point (x-1, y+v)) op)
-                            && y+v >= 0 && y+v <= d) then (Point (x-1, y+v)) else NullPoint) :
-                        (if ((elem (Point (x+1, y+v)) op)
-                            && y+v >= 0 && y+v <= d) then (Point (x+1, y+v)) else NullPoint) : []) pp
+          pm = (if ((elem (Point (x, y+v)) op)
+                   || y+v < 0 || y+v > d) 
+                       then NullPoint else (Point (x, y+v))) :
+               (if ((elem (Point (x-1, y+v)) op)
+                   && y+v >= 0 && y+v <= d) 
+                       then (Point (x-1, y+v)) else NullPoint) :
+               (if ((elem (Point (x+1, y+v)) op)
+                   && y+v >= 0 && y+v <= d) 
+                       then (Point (x+1, y+v)) else NullPoint) : []
 
 -- Get the player's input for a move. I'm not doing a lot of checking on what
 -- the user inputs, that's not the point here. Return a Move datatype.
-getInput :: BoardState -> IO Move
-getInput b@(BoardState (wp, bp) wt d) = do 
+getInput b@(BoardState (wp, bp) wt d) p = do
+    -- Print the board to the screen to show available moves.
+    putStr (show b)
+
     -- Get x,y coordinate of piece to move and make a Point out of it.
     putStr "Enter starting x coordinate: "
-    sx <- getLine 
+    sx <- getLine
     let xc = read sx::Int
     putStr "Enter starting y coordinate: "
     sy <- getLine
@@ -113,23 +127,23 @@ getInput b@(BoardState (wp, bp) wt d) = do
     ey <- getLine
     let yc = read ey::Int
     let ep = Point (xc, yc)
-    return (Move (sp, ep))
+    if (not (elem sp pp))
+       ||  (not (elem ep (legalMoves op p d sp))) then getInput b p
+    else return (Move (sp, ep))
+        where op = if p == 'W' then bp else wp
+              pp = if p == 'W' then wp else bp
 
--- Get the player's Move and create a new BoardState with it. There is no error
--- checking on what the user inputs, that's not the point here.
-playerMove :: BoardState -> BoardState
-playerMove b@(BoardState (wp, bp) wt d) = applyMove b um 'W'
-        where um = liftIO (getInput b)
+-- Get the player's Move and create a new BoardState with it.
+playerMove b p = applyMove b p um
+        where um = unsafePerformIO (getInput b p)
 
 -- Apply the given move to the given BoardState.
-applyMove :: BoardState -> Move -> Char -> BoardState
-applyMove (BoardState (wp, bp) wt d) (Move (s, e)) p
-    | elem e op && p == 'W' = BoardState (e:(deleteMove wp s), deleteMove bp e) nwt d
-    | elem e op && p == 'B' = BoardState (deleteMove wp e, e:(deleteMove bp s)) nwt d
+applyMove (BoardState (wp, bp) wt d) p (Move (s, e))
+    | elem e bp && p == 'W' = BoardState (e:(deleteMove wp s), deleteMove bp e) nwt d
+    | elem e wp && p == 'B' = BoardState (deleteMove wp e, e:(deleteMove bp s)) nwt d
     | p == 'W'              = BoardState (e:(deleteMove wp s), bp) nwt d
     | otherwise             = BoardState (wp, e:(deleteMove bp s)) nwt d
-        where nwt = if wt then False else True
-              op  = if p == 'W' then bp else wp
+        where nwt = not wt
 
 -- Return a list of Moves not including the given Move. If the Move isn't in the
 -- list, just return the list.
@@ -141,8 +155,7 @@ deleteMove ms m
 
 -- Minimax.
 minimax :: BoardState -> BoardState
-minimax (BoardState (wp, bp) wt d) = BoardState (wp, bp) wt d
-
+minimax b@(BoardState (wp, bp) wt d) = b
 
 
 
