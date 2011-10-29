b95ec440a69356b8c755a1d8a245bf9e8f0ce985
Jonathan <ksk.logan@hotmail.com>
2011-10-28 23:15:00 -0700

    Cleaned up the code, ready to implement negascout.

--- a3.hs
+++ a3.hs
@@ -1,5 +1,5 @@
--- Learning Haskell's I/O is not trivial, and it's not the point of this
--- assignment, so use a little "evil" for the greater good.
+-- Learning Haskell's I/O is not trivial, and it's not the point of this assignment, so use a 
+-- little "evil" for the greater good.
 import System.IO.Unsafe
 
 -- Point is an x-y coordinate on the board.
@@ -12,156 +12,176 @@ py (Point (_, y)) = y
 -- Move datatype stores x-y coordinate of starting and ending positions.
 data Move = Move (Point, Point) deriving (Show, Eq)
 
--- BoardState stores a list of white's pieces as points and a list of
--- black's pieces as a tuple, a boolean flag indicating whether or not it
--- is white's turn to move, and an Int for the dimension of the board 
--- minus 1, which is useful for printing the board.
+-- BoardState stores a list of white's pieces as points and a list of black's pieces as a tuple, a 
+-- boolean flag indicating whether or not it is white's turn to move, and an Int for the dimension 
+-- of the board minus 1, which is useful for printing the board.
 data BoardState = BoardState ( [Point], [Point] ) Bool Int
 
--- Print the board to the screen using a helper function. ap is a list of
--- all possible Points on the board. pp creates a list of Strings where
--- each String is either "W", "B" or "_" based on whether the point contains
--- a white pawn, black pawn or is empty, respectively.
+-- Print the board to the screen using a helper function. ap is a list of all possible Points on the 
+-- board. pp creates a list of Strings where each String is either "W", "B" or "_" based on whether 
+-- the point contains a white pawn, black pawn or is empty, respectively.
 instance Show BoardState where
-    show (BoardState (wp, bp) wt d) =
-        (printBoard pp d d) ++ "\n" 
-        where ap = [Point (x, y) | y <- reverse [0..d], x <- [0..d]]
-              pp = [if (elem p wp || elem p bp)
-                        then (if elem p wp then "W"
-                                           else "B")
-                        else "_" | p <- ap]
+    show (BoardState (whitePieces, blackPieces) whitesTurn dimension) =
+        (printBoard playerPieces dimension dimension) ++ "\n" 
 
--- Given a list of Strings from Show BoardState, recursively iterate through
--- the list and append newlines after N elements on a line. This makes
--- printing the board to the screen more user-friendly, as each line contains
--- the pieces on that line.
-printBoard ps n d
-    | null (tail ps) = head ps
-    | n == 0         = head ps ++ "\n" ++ printBoard (tail ps) d d
-    | otherwise      = head ps ++ printBoard (tail ps) (n - 1) d
+        where allPoints    = [Point (x, y) | y <- reverse [0..dimension], x <- [0..dimension]]
+              playerPieces = [if (elem point whitePieces || elem point blackPieces)
+                                then (if elem point whitePieces then "W"
+                                                                else "B")
+                                else "_" | point <- allPoints]
 
--- Store the utility of a BoardState in terms of the AI. We use this for
--- minimax. Printing is mostly for debugging.
+-- Given a list of Strings from Show BoardState, recursively iterate through the list and append 
+-- newlines after N elements on a line. This makes printing the board to the screen more user 
+-- friendly, as each line contains the pieces on that line.
+printBoard points currentVal dimension 
+    | null (tail points) = head points 
+    | currentVal == 0    = head points ++ "\n" ++ printBoard (tail points) dimension dimension
+    | otherwise          = head points ++ printBoard (tail points) (currentVal - 1) dimension
+
+-- Store the utility of a BoardState in terms of the AI. We use this for search. Printing is mostly 
+-- for debugging.
 data Utility = Utility Int deriving (Show, Eq)
 
 -- Generate a NxN board to start the game with.
 genBoard n 
     | n <= 2    = error "Board size is too small."
-    | otherwise = BoardState (white, black) True (n - 1)
-        where xs    = [0..n - 1]
-              white = [Point (x, n - 1) | x <- xs] 
-              black = [Point (x, 0) | x <- xs]
+    | otherwise = BoardState (whitePieces, blackPieces) True (n - 1)
+        where legalRange  = [0..n - 1]
+              whitePieces = [Point (x, n - 1) | x <- legalRange] 
+              blackPieces = [Point (x, 0) | x <- legalRange]
 
--- Play hexapawn with given board. If the game is over, print the winner.
--- Otherwise, get either the user's move or the AIs move.
+-- Play hexapawn with given board. If the game is over, print the winner. Otherwise, get either the 
+-- user's move or the AIs move.
 test :: BoardState -> String
-test b@(BoardState ps wt d)
-    | boardUtility b == 999   = "Black wins."
-    | boardUtility b == -999  = "White wins."
-    | wt == True              = hexapawn (playerMove b (if wt then 'W' else 'B'))
-    | otherwise               = hexapawn (playerMove b (if wt then 'W' else 'B'))
-    
-hexapawn :: BoardState -> String
-hexapawn b@(BoardState ps wt d)
-    | boardUtility b == 999   = "Black wins."
-    | boardUtility b == -999  = "White wins."
-    | wt == True              = hexapawn (playerMove b 'W')
-    | otherwise               = hexapawn (minimax b)
+test board@(BoardState _ whitesTurn _)
+    | boardUtility board == 999   = "Black wins."
+    | boardUtility board == -999  = "White wins."
+    | whitesTurn == True          = test (playerMove board player)
+    | otherwise                   = test (playerMove board player)
+        where player = if whitesTurn then 'W' else 'B'
 
--- Start a game of hexapawn on a NxN board. Disallow boards smaller than
--- 3x3 and larger than 6x6 for now.
+-- Play a game of hexapawn. The BoardState passed in should be the initial BoardState, created by
+-- the play function. This will continue to get either the user's move or the AI's move and switch
+-- players until the game is over.
+hexapawn :: BoardState -> String
+hexapawn board@(BoardState _ whitesTurn dimension)
+    | boardUtility board == 999   = "Black wins."
+    | boardUtility board == -999  = "White wins."
+    | whitesTurn == True          = hexapawn (playerMove board 'W')
+    | otherwise                   = hexapawn (aiMove board depth alpha beta)
+        where   depth = dimension
+                alpha = -999
+                beta  = 999
+
+-- Start a game of hexapawn on a NxN board. Disallow boards smaller than 3x3 and larger than 6x6 for 
+-- now.
 play n 
     | n < 3     = error "Board size too small, minimum is 3x3."
-    | n > 5     = error "Board size is too large, maximum is 5x5."
-    | otherwise = hexapawn (genBoard n)
+    | otherwise = test (genBoard n)
 
--- Return the utility of the board for black. This can be used to determine 
--- whether the game is over, since if the result is BWIN or WWIN, the game has
--- been won by one of the players. Otherwise, it returns the utility of the board
--- for black, the AI, used in minimax.
-boardUtility (BoardState (wp, bp) wt d)
-    | null wp                             = 999
-    | null bp                             = -999
-    | not (null [p | p <- bp, py p == d]) = 999
-    | not (null [p | p <- wp, py p == 0]) = -999
-    | null (map (legalMoves bp 'W' d) wp) = 999
-    | null (map (legalMoves wp 'B' d) bp) = -999
-    | otherwise                           = 0
+-- Return the utility of the board for black. This can be used to determine whether the game is over, 
+-- since if the result is BWIN or WWIN, the game has been won by one of the players. Otherwise, it 
+-- returns the utility of the board for black, the AI, used in search.
+boardUtility (BoardState (whitePieces, blackPieces) whitesTurn dimension)
+    | null whitePieces                                                 = 999
+    | null blackPieces                                                 = -999
+    | not (null [point | point <- blackPieces, py point == dimension]) = 999
+    | not (null [point | point <- whitePieces, py point == 0])         = -999
+    | null (map (legalMoves blackPieces 'W' dimension) whitePieces)    = 999
+    | null (map (legalMoves whitePieces 'B' dimension) whitePieces)    = -999
+    | otherwise                                                        = 0
 
+-- Return a list of legal Points that the Point canmove to. This function assumes the Point contains a
+-- piece belonging to the player. There are three possible Points that can be moved to:
+--  1) Forward one space: The same x-coordinate, y-coordinate plus verticalOffset.
+--  2) Forward one space, one space right: x-coordinate plus one, y-coordinate plus verticalOffset. 
+--  3) Forward one space, one space left: x-coordinate minus one, y-coordinate plus verticalOffset.
+-- The verticalOffset is -1 for white (since white can only move down) and 1 for black.
+legalMoves opponentPieces player dimension (Point (x,y)) = 
+    [point | point <- possibleMoves, point /= NullPoint]
+        where   verticalOffset = if player == 'W' then (-1) else (1)
+                possibleMoves  = (if ((elem (Point (x, y + verticalOffset)) opponentPieces)
+                                    || y + verticalOffset < 0 || y + verticalOffset > dimension) 
+                                        then NullPoint else (Point (x, y + verticalOffset))) :
+                                (if ((elem (Point (x - 1, y + verticalOffset)) opponentPieces)
+                                    && y + verticalOffset >= 0 && y + verticalOffset <= dimension) 
+                                        then (Point (x - 1, y + verticalOffset)) else NullPoint) :
+                                (if ((elem (Point (x + 1, y + verticalOffset)) opponentPieces)
+                                    && y + verticalOffset >= 0 && y + verticalOffset <= dimension) 
+                                        then (Point (x + 1, y + verticalOffset)) else NullPoint) : []
 
--- Return a list of all legal moves that can be made by the given player on
--- the given BoardState. ap is a list of all Points on the given board and
--- am is all the points not occupied.
-legalMoves op pl d (Point (x,y)) = [p | p <- pm, p /= NullPoint]
-    where v  = if pl == 'W' then (-1) else (1)
-          pm = (if ((elem (Point (x, y+v)) op)
-                   || y+v < 0 || y+v > d) 
-                       then NullPoint else (Point (x, y+v))) :
-               (if ((elem (Point (x-1, y+v)) op)
-                   && y+v >= 0 && y+v <= d) 
-                       then (Point (x-1, y+v)) else NullPoint) :
-               (if ((elem (Point (x+1, y+v)) op)
-                   && y+v >= 0 && y+v <= d) 
-                       then (Point (x+1, y+v)) else NullPoint) : []
-
--- Get the player's input for a move. I'm not doing a lot of checking on what
--- the user inputs, that's not the point here. Return a Move datatype.
-getInput b@(BoardState (wp, bp) wt d) p = do
-    -- Print the board to the screen to show available moves.
-    putStr (show b)
+-- Ask the user for their move in canonical form, i.e. (0,0) is the bottom right of the board and
+-- values increase left and up. This function checks the user's move is legal on the given board, and
+-- repeatedly asks for a Move if the input is illegal.
+getInput board@(BoardState (whitePieces, blackPieces) whitesTurn dimension) player = do
+    -- Print the board to the screen to show available moves. Remind the user to use canonical form.
+    putStr ("* Use canonical form, where (0,0) is bottom\nleft and values increase right and up!\n")
+    putStr ("Here's the board:\n" ++ (show board))
 
     -- Get x,y coordinate of piece to move and make a Point out of it.
     putStr "Enter starting x coordinate: "
-    sx <- getLine
-    let xc = read sx::Int
+    x <- getLine
+    let sourceX = read x::Int
     putStr "Enter starting y coordinate: "
-    sy <- getLine
-    let yc = read sy::Int
-    let sp = Point (xc, yc)
+    y <- getLine
+    let sourceY = read y::Int
+    let sourcePoint = Point (sourceX, sourceY)
 
     -- Get x,y coordinate of square to move to and make Point out of it.
     putStr "Enter ending x coordinate: "
-    ex <- getLine
-    let xc = read ex::Int
+    x <- getLine
+    let targetX = read x::Int
     putStr "Enter ending y coordinate: "
-    ey <- getLine
-    let yc = read ey::Int
-    let ep = Point (xc, yc)
-    if (not (elem sp pp))
-       ||  (not (elem ep (legalMoves op p d sp))) then getInput b p
-    else return (Move (sp, ep))
-        where op = if p == 'W' then bp else wp
-              pp = if p == 'W' then wp else bp
+    y <- getLine
+    let targetY = read y::Int
+    let targetPoint = Point (targetX, targetY)
 
--- Get the player's Move and create a new BoardState with it.
-playerMove b p = applyMove b p um
-        where um = unsafePerformIO (getInput b p)
+    -- If the move is illegal, recurse. Otherwise return the Move.
+    if (not (elem sourcePoint playersPieces)) 
+        ||  (not (elem targetPoint (legalMoves opponentsPieces player dimension sourcePoint))) 
+            then getInput board player
+            else return (Move (sourcePoint, targetPoint))
+                where   opponentsPieces = if player == 'W' then blackPieces else whitePieces
+                        playersPieces   = if player == 'W' then whitePieces else blackPieces 
 
--- Apply the given move to the given BoardState.
-applyMove (BoardState (wp, bp) wt d) p (Move (s, e))
-    | elem e bp && p == 'W' = BoardState (e:(deleteMove wp s), deleteMove bp e) nwt d
-    | elem e wp && p == 'B' = BoardState (deleteMove wp e, e:(deleteMove bp s)) nwt d
-    | p == 'W'              = BoardState (e:(deleteMove wp s), bp) nwt d
-    | otherwise             = BoardState (wp, e:(deleteMove bp s)) nwt d
-        where nwt = not wt
+-- Take the user's move and create a new BoardState. This uses the "evil" unsafePerformIO function.
+-- I choose to spend more time on the AI and less on learning Haskell's IO.
+playerMove board player = applyMove board player usersMove
+        where usersMove = unsafePerformIO (getInput board player)
 
--- Return a list of Moves not including the given Move. If the Move isn't in the
--- list, just return the list.
+-- Apply the the Move to the BoardState and return the new BoardState. There are four cases to check:
+--  1) White captures a black pawn: Remove white pawn from source and move into target the black pawn.
+--  2) Black captures a white pawn: The converse of (1).
+--  3) White moves a pawn straight forward: Remove white pawn from source and move into target.
+--  4) Black moves a pawn straight forward: The converse of (3).
+-- This function assumes the Move is legal on the BoardState, it does not perform error checking.
+applyMove (BoardState (whitePieces, blackPieces) whitesTurn dimension) player (Move (source, target))
+    | player == 'W' && elem target blackPieces 
+        = BoardState (target : (deleteMove whitePieces source), 
+            (deleteMove blackPieces source)) notWhitesTurn dimension
+    | player == 'B' && elem target whitePieces
+        = BoardState ((deleteMove whitePieces target), 
+            target : (deleteMove blackPieces source)) notWhitesTurn dimension
+    | player == 'W' 
+        = BoardState ((target : (deleteMove whitePieces source)),
+            blackPieces) notWhitesTurn dimension
+    | otherwise 
+        = BoardState (whitePieces, (target : (deleteMove blackPieces source))) 
+            notWhitesTurn dimension 
+        
+        where notWhitesTurn = not whitesTurn 
+
+-- Take a list of points (the whitePieces or blackPieces from a BoardState) and a Point and return the
+-- list of points excluding the given Point. This is used by applyMove, since white/blackPieces change
+-- on each iteration of the game.
 deleteMove :: [Point] -> Point -> [Point]
-deleteMove ms m
-    | null ms      = []
-    | m == head ms = deleteMove (tail ms) m
-    | otherwise    = (head ms) : deleteMove (tail ms) m
+deleteMove allPoints oldPoint
+    | null allPoints                = []
+    | oldPoint == head allPoints    = deleteMove (tail allPoints) oldPoint
+    | otherwise                     = (head allPoints) : deleteMove (tail allPoints) oldPoint
 
--- Minimax.
-minimax :: BoardState -> BoardState
-minimax b@(BoardState (wp, bp) wt d) = b
-
-
-
-
-
-
-
-
+-- Get the AI's best move on the BoardState. I have implemented negascout, and it can be tweaked from
+-- hexapawn, the calling function, to search a certain depth. By default, it searches the full depth of
+-- the game tree. The initial alpha and beta values are -999 and 999, respectively.
+aiMove board@(BoardState (whitePieces, blackPieces) whitesTurn dimension) depth alpha beta = board 
 
